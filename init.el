f135b182d99a5af9bf880d0b9a08510db6e2fd41
Jonathon Goldsmith <jonathon.goldsmith@offerpop.com>
2016-10-18 14:17:38 -0400

    Move settings from user-init into user-config

--- init.el
+++ init.el
@@ -309,15 +309,6 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
-  (setq-default
-   js-indent-level 2 ;; for json
-   js2-mode-show-parse-errors nil
-   js2-mode-show-strict-warnings nil
-   js2-strict-missing-semi-warning nil
-   js2-strict-trailing-comma-warning nil
-   evil-escape-key-sequence "tn"
-   git-magit-status-fullscreen t
-   ispell-program-name "/usr/local/bin/ispell")
   (add-hook 'markdown-mode-hook 'auto-fill-mode)
 )
 
@@ -328,10 +319,20 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place you code here."
+  (setq-default
+   js-indent-level 2 ;; for json
+   js2-mode-show-parse-errors nil
+   js2-mode-show-strict-warnings nil
+   js2-strict-missing-semi-warning nil
+   js2-strict-trailing-comma-warning nil
+   evil-escape-key-sequence "tn"
+   git-magit-status-fullscreen t
+   ispell-program-name "/usr/local/bin/ispell"
+   persistent-scratch-save-file (expand-file-name ".persistent-scratch" user-home-directory))
   (spacemacs/toggle-automatic-symbol-highlight-on)
   (persistent-scratch-setup-default)
   (server-start)
-  )
+)
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
